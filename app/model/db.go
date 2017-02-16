fa36cfbd6aa9211f328f578d447125dd5002c4d0
justpolidor <justpolidor@gmail.com>
2017-02-16 13:24:12 +0100

    Port fix

--- app/model/db.go
+++ app/model/db.go
@@ -6,7 +6,7 @@ import (
 
 func NewClient() *redis.Client{
 	client := redis.NewClient(&redis.Options{
-		Addr:     "[redis://redistogo:7e7dc2e978d297c6851fcd8344f46b39@porgy.redistogo.com]:10051",
+		Addr:     "redis://redistogo:7e7dc2e978d297c6851fcd8344f46b39@porgy.redistogo.com:10051",
 		Password: "7e7dc2e978d297c6851fcd8344f46b39",
 		DB:       1,  // DB
 	})
