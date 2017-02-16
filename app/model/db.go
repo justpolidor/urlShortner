981fdd7d23c630e36761c68554521e99218d5da8
justpolidor <justpolidor@gmail.com>
2017-02-16 19:41:45 +0100

    Mock credentials

--- app/model/db.go
+++ app/model/db.go
@@ -6,9 +6,9 @@ import (
 
 func NewClient() *redis.Client{
 	client := redis.NewClient(&redis.Options{
-		Addr:     "porgy.redistogo.com:10051",
-		Password: "7e7dc2e978d297c6851fcd8344f46b39",
-		DB:       0,  // DB
+		Addr:     "address",
+		Password: "password",
+		DB:       0,
 	})
 	return client
 }
